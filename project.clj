78af4f50f14c0acb9ac2841312aa0b97ee7549d2
Rick Moynihan <rick.m@swirrl.com>
2014-12-12 16:59:07 +0000

    Version bump plugin

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject grafter "0.1.0-SNAPSHOT"
+(defproject lein-grafter "0.3.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[net.sf.corn/corn-cps "1.1.7"]]
+  ;:source-paths ["./src" "./test-project/src"]
+  :dependencies [[grafter/grafter.pipeline "0.3.0-SNAPSHOT"]]
   :eval-in-leiningen true)
