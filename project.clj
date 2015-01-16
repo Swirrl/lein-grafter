a209f26302902beba22305c1e8c2167340647583
Rick Moynihan <rick.m@swirrl.com>
2015-01-16 16:37:49 +0000

    Updated version number to match Grafter.  Added url/description to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-grafter "0.3.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A leiningen plugin for finding and running grafter pipelines from the commandline."
+  :url "https://github.com/Swirrl/lein-grafter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
