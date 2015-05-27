7d78b1046117cee45667b77e9068b4b8c68b5cb7
Rick Moynihan <rick.m@swirrl.com>
2015-05-27 10:22:01 +0100

    Update to grafter 0.5.0-SNAPSHOT

--- src/leiningen/grafter.clj
+++ src/leiningen/grafter.clj
@@ -10,7 +10,7 @@
   (:import [net.sf.corn.cps CPScanner ResourceFilter]
            [java.io InputStreamReader PushbackReader FileNotFoundException]))
 
-(def grafter-profile {:dependencies '[[lein-grafter "0.4.0"]
+(def grafter-profile {:dependencies '[[lein-grafter "0.5.0-SNAPSHOT"]
                                       [leiningen "2.5.0"]]})
 
 (def grafter-requires '(do (require 'grafter.pipeline)
