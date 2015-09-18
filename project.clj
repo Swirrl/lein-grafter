8049066d609f20459fa1dd47cda1891d172ab014
Rick Moynihan <rick.m@swirrl.com>
2015-09-18 15:36:14 +0100

    version bump grafter/plugin to 0.6.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject lein-grafter "0.5.0"
+(defproject lein-grafter "0.6.0-SNAPSHOT"
   :description "A leiningen plugin for finding and running grafter pipelines from the commandline."
   :url "https://github.com/Swirrl/lein-grafter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[grafter "0.5.0"]]
+  :dependencies [[grafter "0.6.0-SNAPSHOT"]]
   :eval-in-leiningen true)
