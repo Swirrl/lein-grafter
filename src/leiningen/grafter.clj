8a0b2919cf5fce63c1819bf23936716b75b330d0
Rick Moynihan <rick.m@swirrl.com>
2015-09-18 15:37:18 +0100

    Make grafter list work with new style of pipelines
    TODO: Make grafter run work with new style of pipelines

--- src/leiningen/grafter.clj
+++ src/leiningen/grafter.clj
@@ -1,19 +1,20 @@
 (ns leiningen.grafter
   (:refer-clojure :exclude [list])
   (:require
-   [grafter.pipeline :refer [find-clj-classpath-resources find-resource-pipelines
-                             fully-qualified-name apply-pipeline]]
+   ;[grafter.pipeline :refer [apply-pipeline]]
    [leiningen.core.main :refer [info warn debug abort]]
    [leiningen.core.eval :refer [eval-in-project]]
    [leiningen.core.project :as project]
    [clojure.string :as string])
-  (:import [net.sf.corn.cps CPScanner ResourceFilter]
+  (:import
            [java.io InputStreamReader PushbackReader FileNotFoundException]))
 
 (def grafter-profile {:dependencies '[[lein-grafter "0.5.0"]
                                       [leiningen "2.5.0"]]})
 
-(def grafter-requires '(do (require 'grafter.pipeline)
+(def grafter-requires '(do (require 'clojure.java.io)
+                           (require 'grafter.pipeline)
+                           (require 'clojure.edn)
                            (require 'grafter.pipeline.plugin)
                            (require 'grafter.rdf)
                            (require 'grafter.rdf.io)
@@ -28,35 +29,48 @@
 (defn list
   "Lists all the grafter pipelines in the current project"
   ([project type]
-   (let [function-call-form (clojure.core/list (case type
-                                                 nil 'grafter.pipeline.plugin/list-pipelines
-                                                 "pipes" 'grafter.pipeline.plugin/list-pipes
-                                                 "grafts" 'grafter.pipeline.plugin/list-grafts
-                                                 (leiningen.core.main/abort "Usage: lein grafter list [pipes|grafts|pipelines]")))]
-     (eval-in-grafter-project project `(doseq [pipeline-desc# ~function-call-form]
-                                         (leiningen.core.main/info pipeline-desc#))
+   (let [function-call-form (if (or (nil? type) (#{"pipe" "graft"} type))
+                              `(grafter.pipeline.plugin/list-pipelines ~type)
+                              (leiningen.core.main/abort "Usage: lein grafter list [pipe|graft]"))]
+     (eval-in-grafter-project project `(do
+                                         (let [config# (clojure.edn/read-string (slurp "grafter-config.edn"))]
+                                           (apply require (:pipeline-namespaces config#))
+                                           (doseq [pipeline-desc# ~function-call-form]
+                                             (leiningen.core.main/info pipeline-desc#))))
                               grafter-requires))))
+
+
+
 
 (defn run
   "Run the specified grafter pipeline"
   [project pipeline inputs output]
 
-  (eval-in-grafter-project project
-                           `(do (try
-                                  (let [results# (grafter.pipeline/apply-pipeline ~pipeline '~inputs)]
-                                    (if (re-find #"\.(xls|xlsx|csv)$" ~output)
-                                      (grafter.tabular/write-dataset ~output results#)
-                                      (if (grafter.tabular/dataset? results#)
-                                        (leiningen.core.main/abort (str "Could not write RDF as " (class results#)
-                                                                        " is not a valid representation of Statements.  This error usually occurs if you try and generate graph data from a pipe."))
-                                        (grafter.rdf/add (grafter.rdf.io/rdf-serializer ~output) results#))))
-                                  (leiningen.core.main/info (str '~@inputs " --[" ~pipeline "]--> " ~output))
-                                  (catch FileNotFoundException ex#
-                                    (leiningen.core.main/abort (str "No such pipeline " ~pipeline " pipelines must be defined with defpipeline to be found by this plugin")))))
+  (let [syntree `(do
+                   (let [config# (clojure.edn/read-string (slurp "grafter-config.edn"))]
+                     (apply require (:pipeline-namespaces config#))
 
-                           (concat grafter-requires '((require 'grafter.tabular)
-                                                      (require 'grafter.tabular.csv)
-                                                      (require 'grafter.tabular.excel)))))
+                     (try
+                       ;; TODO need to interpret parameters against their types
+                       ;; and call e.g. read-dataset if it's a dataset this
+                       ;; should use code in grafter.pipeline.types
+                       (let [results# ((symbol ~pipeline) ~@inputs)]
+                         (leiningen.core.main/info (str ~@inputs " --[" ~pipeline "]--> " ~output " results " results#))
+                         (if (re-find #"\.(xls|xlsx|csv)$" ~output)
+                           (if (grafter.tabular/dataset? results#)
+                             (grafter.tabular/write-dataset ~output results#)
+
+                             (grafter.rdf/add (grafter.rdf.io/rdf-serializer ~output) results#))))
+
+                       (catch FileNotFoundException ex#
+                         (leiningen.core.main/abort (str "No such pipeline " ~pipeline " pipelines must be defined with defpipeline to be found by this plugin"))))))]
+    (prn syntree)
+    (eval-in-grafter-project project
+                             syntree
+
+                             (concat grafter-requires '((require 'grafter.tabular)
+                                                        (require 'grafter.tabular.csv)
+                                                        (require 'grafter.tabular.excel))))))
 
 (defn ^{:subtasks [#'list #'run]} grafter
   "Plugin for managing and executing grafter pipelines."
