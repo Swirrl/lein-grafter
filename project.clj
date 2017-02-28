b4877fcb7b69627552de525fee3b0d4b7bda9bee
Rick Moynihan <rick.m@swirrl.com>
2017-02-28 13:15:51 +0000

    Bump grafter dependency to 0.8.6

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[grafter "0.7.4"]]
+  :dependencies [[grafter "0.8.6"]]
   :eval-in-leiningen true)
