5df49c3d53e4c97ce2aaaffd5907c4538c6ad0de
Rick Moynihan <rick.m@swirrl.com>
2017-03-16 17:50:10 +0000

    Version 0.8.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-grafter "0.8.7-SNAPSHOT"
+(defproject lein-grafter "0.8.7"
   :description "A leiningen plugin for finding and running grafter pipelines from the commandline."
   :url "https://github.com/Swirrl/lein-grafter"
   :license {:name "Eclipse Public License"
